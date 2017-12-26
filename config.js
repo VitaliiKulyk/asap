ea2a3777775e74a3e89438b800d50772f1bd2e38
Vitalii <vetalkulyk@gmail.com>
2017-12-27 01:45:42 +0200

    '1.0'

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-	port: 8800,
+	port: 3000,
 	dbConnectionString: 'postgres://jkhhotha:3TKXXLNhfGW9li0LuFIR9Cpvnf87TGeb@horton.elephantsql.com:5432/jkhhotha',
 	saltRounds: 2,
 	jwtSecret: 'yo-its-a-secret',
