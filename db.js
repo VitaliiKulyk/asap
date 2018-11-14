199f8dfe92afe9082c36c5a95495a398eacc2738
Tulasireddyv <thulasi250@gmail.com>
2018-11-14 17:33:10 +0530

    Update db.js
    Adding db config changes

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 const config = require('./config');
 const Sequelize = require('sequelize');
-var sequelize = new Sequelize(config.dbConnectionString);
+var sequelize = new Sequelize(config.database, config.type, config.name, config.myPostgres);
 require('sequelize-values')(sequelize);
 
 module.exports = sequelize;
